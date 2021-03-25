bb42b560bfd8e2a82a973cfe048d3b1148ca030f
Danilo Del Busso <danilo.delbusso1@gmail.com>
2021-03-25 23:12:55 +0100

    Add heatmap caching

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fetch = require('node-fetch');
-const DEFAULT_API_ENDPOINT = "http://3.8.139.11:4683/api/v1/wifis/get/reloadFeatureCache"
+const DEFAULT_API_ENDPOINT_FEATURES = "http://localhost:4683/api/v1/wifis/get/reloadFeatureCache"
+const DEFAULT_API_ENDPOINT_HEAT_MAP = "http://localhost:4683/api/v1/wifis/get/realoadHeatmapData"
 
-const startRequest = async (event) => {
-  fetch(process.env.API_ENDPOINT ?? DEFAULT_API_ENDPOINT);
+const startRequest = async () => {
+  fetch(process.env.API_ENDPOINT ?? DEFAULT_API_ENDPOINT_FEATURES);
+  fetch(process.env.API_ENDPOINT ?? DEFAULT_API_ENDPOINT_HEAT_MAP);
   const response = {
     statusCode: 200,
     body: JSON.stringify('Request started.'),
