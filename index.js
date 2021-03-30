9384b41ffec3d9627e22462240a12777f86b6f4f
Danilo Del Busso <danilo.delbusso1@gmail.com>
2021-03-30 22:59:17 +0200

    Apply ESLint autofix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
-const DEFAULT_API_ENDPOINT_FEATURES = "http://localhost:4683/api/v1/wifis/get/reloadFeatureCache"
-const DEFAULT_API_ENDPOINT_HEAT_MAP = "http://localhost:4683/api/v1/wifis/get/realoadHeatmapData"
+const DEFAULT_API_ENDPOINT_FEATURES = 'http://localhost:4683/api/v1/wifis/get/reloadFeatureCache';
+const DEFAULT_API_ENDPOINT_HEAT_MAP = 'http://localhost:4683/api/v1/wifis/get/realoadHeatmapData';
 
 const startRequest = async () => {
   fetch(process.env.API_ENDPOINT ?? DEFAULT_API_ENDPOINT_FEATURES);
