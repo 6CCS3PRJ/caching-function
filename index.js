1438231a22e8f2f8c40788c71cb3c89fa581a351
Danilo Del Busso <danilo.delbusso1@gmail.com>
2021-04-05 14:09:03 +0200

    Update endpoints

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const fetch = require('node-fetch');
-const DEFAULT_API_ENDPOINT_FEATURES = 'http://localhost:4683/api/v1/wifis/get/reloadFeatureCache';
-const DEFAULT_API_ENDPOINT_HEAT_MAP = 'http://localhost:4683/api/v1/wifis/get/realoadHeatmapData';
+const DEFAULT_API_ENDPOINT_FEATURES = 'http://localhost:4683/api/v1/wifis/patch/reloadFeatureCache';
+const DEFAULT_API_ENDPOINT_HEAT_MAP = 'http://localhost:4683/api/v1/wifis/patch/reloadHeatmapData';
 
 const startRequest = async () => {
-  fetch(process.env.API_ENDPOINT ?? DEFAULT_API_ENDPOINT_FEATURES);
-  fetch(process.env.API_ENDPOINT ?? DEFAULT_API_ENDPOINT_HEAT_MAP);
+  fetch(
+      process.env.API_ENDPOINT ?? DEFAULT_API_ENDPOINT_FEATURES,
+      {method: 'PATCH'},
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      },
+  );
+  fetch(
+      process.env.API_ENDPOINT ?? DEFAULT_API_ENDPOINT_HEAT_MAP,
+      {method: 'PATCH'},
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      },
+  );
   const response = {
     statusCode: 200,
     body: JSON.stringify('Request started.'),
   };
   return response;
 };
+
+startRequest()
 
 exports.handler = startRequest;
